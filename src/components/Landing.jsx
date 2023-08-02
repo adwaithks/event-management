ffd41a1a8d44c26ced69554d7023e99e74336717
adwaithks <adwaith2507@gmail.com>
2023-08-02 19:47:20 +0530

    brightness reduce

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -31,7 +31,7 @@ const Landing = () => {
 				<Image
 					sx={{ position: "absolute", zIndex: 1 }}
 					objectFit="cover"
-					filter="brightness(0.3)"
+					filter="brightness(0.2)"
 					src="https://images.unsplash.com/photo-1546893694-adb39267d4a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=627&q=80"
 					height="100%"
 					width="100%"
